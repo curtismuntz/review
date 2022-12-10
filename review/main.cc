73eaa7174f4dc3f64b18bc845bcc50939e9f82cd
murt <me@murt.is>
2022-12-09 20:28:10 -0800

    remove the stupid using namespace

--- review/main.cc
+++ review/main.cc
@@ -1,45 +1,28 @@
-// //Uncomment the following line if you are compiling this code in Visual Studio
-// //#include "stdafx.h"
-
 #include <opencv2/opencv.hpp>
 #include <iostream>
-
-using namespace cv;
-using namespace std;
 
 int main(int argc, char** argv)
 {
  // Read the image file
- Mat image = imread("data/murt_professional.jpg");
+ cv::Mat image = cv::imread("data/murt_professional.jpg");
 
  // Check for failure
  if (image.empty()) 
  {
-  cout << "Could not open or find the image" << endl;
-  cin.get(); //wait for any key press
+  std::cout << "Could not open or find the image" << std::endl;
+  std::cin.get(); //wait for any key press
   return -1;
  }
 
- String windowName = "The Guitar"; //Name of the window
+ cv::String windowName = "The Guitar"; //Name of the window
 
- namedWindow(windowName); // Create a window
+ cv::namedWindow(windowName); // Create a window
 
- imshow(windowName, image); // Show our image inside the created window.
+ cv::imshow(windowName, image); // Show our image inside the created window.
 
- waitKey(0); // Wait for any keystroke in the window
+ cv::waitKey(0); // Wait for any keystroke in the window
 
- destroyWindow(windowName); //destroy the created window
+ cv::destroyWindow(windowName); //destroy the created window
 
  return 0;
 }
-
-
-
-// #include <iostream>
-// #include <boost/filesystem/operations.hpp>
-// #include <zlib.h>
-
-// int main()
-// {
-// 	std::cout << "Hello world!\n";
-// }
